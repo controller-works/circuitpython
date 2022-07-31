80a7d4ae4f2371b199eb407715e38e5a9f7dd1ac
kevinmgee <kevingee@kevingee.biz>
2022-07-31 00:40:34 -0400

    trying to create a new board

---
+++ ports/raspberrypi/boards/controllerworks_mini42_rp2040/mpconfigboard.h
@@ -0,0 +1,8 @@
+#define MICROPY_HW_BOARD_NAME "Controller Works mini42"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO25)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO24)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
